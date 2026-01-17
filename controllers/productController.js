4dd8c96794f2675547b9e144342c51e7000f9f62
Steve George <98937566+salbatti@users.noreply.github.com>
2026-01-17 16:12:07 +0530

    sellerLogic created

---
+++ controllers/productController.js
@@ -0,0 +1,24 @@
+
+//Add Product : /api/product/add
+export const addProduct = async (req ,res)=>{
+
+}
+
+//get Product : /api/product/list/id
+export const productList = async (req ,res)=>{
+
+}
+
+
+//get single  Product : /api/product/list
+export const productById = async (req ,res)=>{
+
+}
+
+
+
+
+//Change product in stock : /api/product/stock
+export const changeStock = async (req ,res)=>{
+
+}

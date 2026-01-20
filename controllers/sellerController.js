99c92a9beea3a338211b386423cd955a61f05000
Steve George <98937566+salbatti@users.noreply.github.com>
2026-01-20 13:20:12 +0530

    sellerLogin backend

--- controllers/sellerController.js
+++ controllers/sellerController.js
@@ -47,6 +47,7 @@ export const sellerLogout = async (req, res) => {
         return res.json({ success: true,message:"Logged out"})
     } catch (error) {
         console.log(error.message);
+        
         res.json({ success: false, message: error.message })
     }
 }

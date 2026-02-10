ab22a7b1cbd29335361229eb107aa0d477e7aef2
Steve George <98937566+salbatti@users.noreply.github.com>
2026-02-10 09:33:00 +0530

    stripe logic

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,7 +1,8 @@
-// Place Order COD : /api/order/cod
 
 import Order from "../models/Order.js"
 import Product from "../models/Product.js"
+import stripe from "stripe"
+// Place Order COD : /api/order/cod
 
 export const placeOrderCOD = async (req, res) => {
     try {
@@ -33,6 +34,74 @@ export const placeOrderCOD = async (req, res) => {
     }
 }
 
+// Place Order Stripe : /api/order/stripe
+
+export const placeOrderStripe = async (req, res) => {
+    try {
+        const { userId, items, address } = req.body
+        const {origin}=req.headers;
+        if (!address || items.length == 0) {
+            return res.json({ success: false, mesaage: "Invalid data" })
+        }
+
+        let productData = []
+
+        //Calculate amount using Items
+        let amount = await items.reduce(async (acc, item) => {
+            const product = await Product.findById(item.product);
+            productData.push({
+                name: product.name,
+                price:product.offerPrice,
+                quantity:item.quantity
+            });
+            return (await acc) + product.offerPrice * item.quantity
+        }, 0)
+
+        //Add Tax Charge(2%)
+        amount += Math.floor(amount * 0.02)
+
+        const order = await Order.create({
+            userId,
+            items,
+            amount,
+            address,
+            paymentType: 'Online'
+        });
+        //Stripe Gateway Initialize
+        const stripeInstance = new stripe (process.env.STRIPE_SECRET_KEY)
+        
+        //create line items for stripe
+
+        const line_items=productData.map((item)=>{
+            return{
+                price_data:{
+                    currency:'usd',
+                    product_data:{
+                        name: item.name,
+                    },
+                    unit_amount: Math.floor(item.price+item.price*0.02)*100
+                },
+                quantity:item.quantity,
+            }
+        })
+
+        // create session
+        const session = await stripeInstance.checkout.sessions.create({
+            line_items,
+            mode:"payment",
+            success_url:`${origin}/loader?next=my-orders`,
+            cancel_url:`${origin}/cart`,
+            metadata:{
+                orderId:order._id.toString(),
+                userId
+            }
+        })
+        return res.json({ success: true,url:session.url })
+    } catch (error) {
+        console.log(error.mesaage);
+        return res.json({ success: false, mesaage: error.mesaage })
+    }
+}
 
 // Get Orders by User ID : /api/order/user
 
